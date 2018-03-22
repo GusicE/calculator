764805e1e1c9ca01005adf2c3e58ac0849749375
EGusic <e.gusic88@gmail.com>
2018-03-22 01:33:22 +0100

    v0.9

--- js/script.js
+++ js/script.js
@@ -6,31 +6,6 @@ class Button {
 		this.value = Text;
 		this.wrapper = Wrapper;
 	}
-
-		getValue() {
-			return this.value;
-		}
-
-		createBtn() {
-			this.element = document.createElement("button");
-			this.element.textContent = this.text;
-
-			let container = document.getElementById(this.wrapper);
-			container.appendChild(this.element);
-		}
-
-		addEvent() {
-			this.element.addEventListener("click", this.showBtn.bind(this), false);
-		}
-
-		showBtn() {
-			alert(this.text);
-		}
-
-		consoleBtn(text) {
-			console.log(this.text);
-		}
-
 }
 
 // Screen class /////////////////////////////////////////////////////////////
@@ -38,63 +13,171 @@ class Screen {
 
 	constructor(id) {
 		this.screen = document.getElementById(id);
+		this.calcScreen;
+		this.valueScreen;
+		this.storedValue = [];
 	}
 
-	showValue(value) {
-		this.element.textContent += this.text
+	addNumber(value) {
+		if(this.storedValue.length > 0) {
+			this.valueScreen.textContent += value;
+			this.storedValue.push(value);
+		} else if(this.storedValue.length == 0) {
+			this.calcScreen.textContent = this.storedValue.join("");
+			this.valueScreen.textContent = value;
+			this.storedValue.push(value);
+		}
+
+		console.log(this.storedValue);
 	}
 
+	addOperator(value) {
+		let OperatorValue = " " + value + " ";
+
+		this.valueScreen.textContent = "";
+		this.storedValue.push(OperatorValue);
+		this.calcScreen.textContent = this.storedValue.join("");
+
+		console.log(this.storedValue);
+	}
+
+	showCalc(value) {
+		let result = eval(this.storedValue.join(""));
+		let cleanStoredValue = this.storedValue.join("");
+
+		this.storedValue += " " + value;
+		this.calcScreen.textContent = cleanStoredValue;
+		this.valueScreen.textContent = result;
+		this.storedValue = [];
+	}
+
+	backspace() {
+		this.storedValue.splice(-1);
+		this.calcScreen.textContent = "";
+		this.valueScreen.textContent = this.storedValue.join("");
+	}
+
+	clear() {
+		this.storedValue = [];
+		this.calcScreen.textContent = "";
+		this.valueScreen.textContent = "";
+	}
 }
 
 // Calculator class /////////////////////////////////////////////////////////
-{
-	createScreen(){
+
+	function createScreen(){
 		let display = new Screen('screen');
+		return display;
 	};
 
-	createButtons(arr) {
-		let buttons = arr
+	function createButtons(arr) {
+		let tempButtons = arr;
+		let buttons = [];
 
-		buttons.forEach(function(element) {
-			element = new Button(element, 'numbers');
-		}
+		tempButtons.forEach(function(btn) {
+			btn = new Button(btn, 'numbers');
+			buttons.push(btn);
+		})
+
+		return buttons;
 	};
 
-	createOperators(arr) {
-		let operators = arr;
+	function createOperators(arr) {
+		let tempOperators = arr;
+		let operators = [];
 
-		operators.forEach(function(element) {
+		tempOperators.forEach(function(element) {
 			element = new Button(element, 'operators');
-		}
+			operators.push(element);
+		})
+
+		return operators;
 	};
+
+
 
 	class Calculator {
 
-		constructor(){
-			this.display = createDisplay();
-			this.array_buttons = createButtons();
-			this.array_operators = createOperators();	
+		constructor(arr1, arr2){
+			this.display = createScreen();
+			this.array_numbers = createButtons(arr1);
+			this.array_operators = createOperators(arr2);
+			this.array_buttons = this.array_numbers.concat(this.array_operators)	
 		}
-}
+
+		addScreen() {
+			let display = this.display;
+			let container = display.screen;
+
+			display.calcScreen = document.createElement("span");
+			display.calcScreen.setAttribute("id", "calcScreen");
+
+			display.valueScreen = document.createElement("span");
+			display.valueScreen.setAttribute("id", "valueScreen");
+
+			container.appendChild(display.calcScreen);
+			container.appendChild(display.valueScreen);		
+		}
+
+		addButtons() {
+			this.array_buttons.forEach(function(btn) {
+				btn.element = document.createElement("button");
+				btn.element.textContent = btn.value;
+				btn.element.setAttribute("id", btn.value);
+
+				let container = document.getElementById(btn.wrapper);
+				container.appendChild(btn.element);
+			})
+		}
+
+		addEvent() {
+			let display = this.display;
+
+			this.array_numbers.forEach(function(btn) {
+				if (btn.value == "<=") {
+					btn.element.addEventListener("click", function() { display.backspace()});
+				} else {
+					btn.element.addEventListener("click", function() { display.addNumber(btn.value)});
+				}
+			});
+
+			this.array_operators.forEach(function(btn) {
+				if (btn.value == "+" || btn.value == "-") {
+					btn.element.addEventListener("click", function() { display.addOperator(btn.value)});
+				} else if (btn.value == "=") {
+					btn.element.addEventListener("click", function() { display.showCalc(btn.value)});
+				} else if (btn.value == "clear") {
+					btn.element.addEventListener("click", function() { display.clear()});
+				}
+			});
+				
+		}
+		
+
+		debug() {
+			console.log(this.display);
+			console.log(this.array_buttons);
+		}
+
+	};
+
 
 // Main function ////////////////////////////////////////////////////////////
 function main() {
 
-	var buttons = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+	var buttons = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.', '<='];
+	var operators = ['clear', '+', '-', '='];
 
-	buttons.forEach(function(element) {
-		element = new Button(element, 'numbers');
-		element.createBtn();
-		element.addEvent();
-	});
+	calc = new Calculator(buttons, operators);
 
-	var operators = ['+', '-', '='];
+	calc.debug();
 
-	operators.forEach(function(element) {
-		element = new Button(element, 'operators');
-		element.createBtn();
-		element.addEvent();
-	})
+	calc.addScreen();
+
+	calc.addButtons();
+
+	calc.addEvent();
 
 }
 
